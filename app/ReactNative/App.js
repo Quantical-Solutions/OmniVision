86724214f883749504637dd8ef9ad37ddc5979f5
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-04 19:24:41 +0100

    update

--- app/ReactNative/App.js
+++ app/ReactNative/App.js
@@ -1,15 +1,21 @@
-import React, { Component, useState } from 'react';
-import { StatusBar } from 'react-native';
-import Url from './includes/components/Url';
+import React, { Component } from 'react';
+import {StatusBar, View, StyleSheet} from 'react-native';
+import {Url} from './includes/components/Url';
 
 export default class App extends Component {
 
     render() {
         return (
-            <>
+            <View style={styles.container}>
                 <StatusBar hidden={false} backgroundColor={'#FFF'} barStyle={'dark-content'} />
                 <Url />
-            </>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
